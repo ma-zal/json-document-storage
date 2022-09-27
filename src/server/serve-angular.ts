35ad74da3d3e01931b373fe3c902c321615940d3
Martin Zaloudek <ma-zal@users.noreply.github.com>
2022-09-27 20:54:00 +0200

    Fixed baseHref

--- src/server/serve-angular.ts
+++ src/server/serve-angular.ts
@@ -5,21 +5,24 @@ import path from 'path';
  * 
  * @param app 
  * @param htmlDir 
- * @param basePath - basepath without '/' on begin and wihout on the end. @example `client`
+ * @param baseHref - must be with '/' on begin and wihout on the end. @example `/client`
  */
-export function serveAngular(app: Application, htmlDir: string, basePath: string) {
-  app.use(basePath, express.static(htmlDir));
+export function serveAngular(app: Application, htmlDir: string, baseHref: string) {
+  if (!baseHref.length || baseHref[0] !== '/') {
+    throw new Error(`Invalid baseHred "${baseHref}". It must to start with "/" characted.`);
+  }
+  app.use(baseHref, express.static(htmlDir));
   // Serve Angular HTML5 routes
-  app.get(angularAllRoutes(basePath), (req, res) => {
+  app.get(angularAllRoutes(baseHref), (req, res) => {
     res.sendFile(path.join(htmlDir, 'index.html'));
   });
   // Redirect root to app
-  app.get(/^\/$/, (req, res) => { res.redirect(basePath + '/'); });
+  app.get(/^\/$/, (req, res) => { res.redirect(baseHref + '/'); });
 }
 
 /**
  * Generates regExp that matches all Angular HTML5 paths.
  */
 export function angularAllRoutes(appBasehref: string) {
-  return new RegExp(`^\/${appBasehref}(\/[a-zA-Z0-9/_-]*)?$`);
+  return new RegExp(`^${appBasehref}(\/[a-zA-Z0-9/_-]*)?$`);
 }
