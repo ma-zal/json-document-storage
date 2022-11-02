c64c0a034208f864a5663fd4a117996d45752f23
Martin Zaloudek <ma-zal@users.noreply.github.com>
2022-11-02 12:20:46 +0100

    Stop webserver on SIGINT.

--- src/server/http-server.ts
+++ src/server/http-server.ts
@@ -1,9 +1,30 @@
 import { Express } from 'express';
+import { Server } from 'http';
 
-/** Start HTTP Webserver */
+let httpServer: Server | undefined;
+
+/** Start HTTP webserver */
 export function startHttpServer(app: Express) {
-  const httpServer = app.listen(3000, () => {
+  if (httpServer) {
+    throw new Error('Webserver is already running.');
+  }
+
+  httpServer = app.listen(3000, () => {
     console.log(`Server is running at http://0.0.0.0:3000`);
   });
-  httpServer.on('close', () => { console.info('HTTP WebServer closed.'); });
+  httpServer.on('close', () => { console.info('HTTP webserver closed.'); });
 }
+
+/** Stop webserver on SIGINT (by docker container stop) */
+process.on('SIGINT', () => {
+  console.log('SIGINT received. Stopping HTTP webserver ...');
+  if (httpServer) {
+    httpServer.close();
+    httpServer = undefined;
+  }
+
+  if (!httpServer) {
+    console.log('HTTP webserver is already stopped. Killing the app forcibly.');
+    process.exit(100);
+  }
+});
